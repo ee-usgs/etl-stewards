bda5f5c72116b6262a5d7146472bcdd875aa8293
Dave Steinich <drsteini@usgs.gov>
2013-12-18 20:24:59 +0000

    parallel & nix nulls

--- buildDwDataTables.sql
+++ buildDwDataTables.sql
@@ -17,23 +17,23 @@ begin
    where translate(table_name, '0123456789', '0000000000') = 'RESULT_00000';
   dbms_output.put_line('new suffix:' || new_suffix); 
   
-  execute immediate 'create table organization' || new_suffix || ' as select * from organization_temp@' || dblink;
+  execute immediate 'create table organization' || new_suffix || ' as select /*+ parallel (4) */ * from organization_temp@' || dblink;
   execute immediate 'alter table organization' || new_suffix || ' add constraint organization' || new_suffix || '_pk primary key (code_value)';
   dbms_output.put_line('created table organization' || new_suffix);
   
-  execute immediate 'create table station' || new_suffix || ' as select * from station_temp@' || dblink;
+  execute immediate 'create table station' || new_suffix || ' as select /*+ parallel (4) */ * from station_temp@' || dblink;
   execute immediate 'alter table station' || new_suffix || ' add constraint station' || new_suffix || '_pk primary key (station_pk)';
   execute immediate 'alter table station' || new_suffix || ' add constraint station' || new_suffix || '_org foreign key (organization_id) references organization' ||
                      new_suffix || ' (code_value) disable';
   dbms_output.put_line('created table station' || new_suffix);
 
-  execute immediate 'create table activity' || new_suffix || ' as select * from activity_temp@' || dblink;
+  execute immediate 'create table activity' || new_suffix || ' as select /*+ parallel (4) */ * from activity_temp@' || dblink;
   execute immediate 'alter table activity' || new_suffix || ' add constraint activity' || new_suffix || '_pk primary key (activity_pk)';
   execute immediate 'alter table activity' || new_suffix || ' add constraint activity' || new_suffix || '_station foreign key (station_pk) references station' ||
                      new_suffix || ' (station_pk) disable';
   dbms_output.put_line('created table activity' || new_suffix);
 
-  execute immediate 'create table result' || new_suffix || ' as select * from result_temp@' || dblink;
+  execute immediate 'create table result' || new_suffix || ' as select /*+ parallel (4) */ * from result_temp@' || dblink;
   execute immediate 'alter table result' || new_suffix || ' add constraint result' || new_suffix || '_pk primary key (result_pk)';
   execute immediate 'alter table result' || new_suffix || ' add constraint result' || new_suffix || '_station foreign key (station_pk) references station' ||
                      new_suffix || ' (station_pk) disable';
--- buildDwLookups.sql
+++ buildDwLookups.sql
@@ -17,23 +17,26 @@ begin
   dbms_output.put_line('using suffix:' || the_suffix); 
 
   execute immediate 'create table characteristicname' || the_suffix || ' as
-                     select value code_value,
-                            descr description,
+                     select code_value,
+                            description,
                             rownum sort_order
-                       from (select distinct characteristic_name value,
-                                    cast(null as varchar2(4000 char)) descr,
-                                    characteristic_name
+                       from (select /*+ parallel (4) */
+                                    distinct characteristic_name code_value,
+                                             cast(null as varchar2(4000 char)) description
                                from result' || the_suffix || '
-                                 order by characteristic_name)';
+                              where characteristic_name is not null
+                                 order by 1)';
   dbms_output.put_line('created table characteristicname' || the_suffix);
 
   execute immediate 'create table samplemedia' || the_suffix || ' as
-                     select value code_value,
-                            descr description,
+                     select code_value,
+                            description,
                             rownum sort_order
-                       from (select distinct sample_media value,
-                                    cast(null as varchar2(4000 char)) descr
+                       from (select /*+ parallel (4) */
+                                    distinct sample_media code_value,
+                                             cast(null as varchar2(4000 char)) description
                                from result' || the_suffix || '
+                              where sample_media is not null
                                  order by 1)';
   dbms_output.put_line('created table samplemedia' || the_suffix);
 
@@ -43,7 +46,8 @@ begin
                             country_cd,
                             state_cd,
                             rownum sort_order
-                       from (select distinct b.code_value,
+                       from (select /*+ parallel (4) */ 
+                                    distinct b.code_value,
                                              b.description,
                                              a.country_cd,
                                              a.state_cd,
@@ -51,20 +55,23 @@ begin
                                from station' || the_suffix || q'! a
                                     join nwis_ws_star.county b
                                       on a.country_cd || ':' || a.state_cd || ':' || a.county_cd = b.code_value
+                              where country_cd is not null
                                 order by country_cd desc,
                                          state_cd,
                                          county_cd)!';
   dbms_output.put_line('created table county' || the_suffix);
 
   execute immediate 'create table country' || the_suffix || ' as
-                     select value code_value,
-                            descr description,
+                     select code_value,
+                            description,
                             rownum sort_order
-                       from (select distinct a.country_cd value,
-                                             b.description descr
+                       from (select /*+ parallel (4) */ 
+                                    distinct a.country_cd code_value,
+                                             b.description description
                                from station' || the_suffix || ' a
                                     join nwis_ws_star.country b
                                       on a.country_cd = b.code_value
+                              where country_cd is not null
                                  order by 2 desc)';
   dbms_output.put_line('created table country' || the_suffix);
 
@@ -74,25 +81,29 @@ begin
                             description_with_out_country,
                             country_cd,
                             rownum sort_order
-                             from (select distinct b.code_value,
-                                          b.description_with_country,
-                     b.description_with_out_country,
-                                          a.country_cd,
-                                          a.state_cd
-                                     from station' || the_suffix || q'! a
-                                          join nwis_ws_star.state b
-                                            on a.country_cd || ':' ||a.state_cd = b.code_value
-                                      order by country_cd desc,
+                       from (select /*+ parallel (4) */ 
+                                    distinct b.code_value,
+                                             b.description_with_country,
+                                             b.description_with_out_country,
+                                             a.country_cd,
+                                             a.state_cd
+                               from station' || the_suffix || q'! a
+                                    join nwis_ws_star.state b
+                                      on a.country_cd || ':' ||a.state_cd = b.code_value
+                              where country_cd is not null
+                                 order by country_cd desc,
                                                state_cd)!';
   dbms_output.put_line('created table state' || the_suffix);
 
   execute immediate 'create table sitetype' || the_suffix || ' as
-                     select value code_value,
-                            descr description,
+                     select code_value,
+                            description,
                             rownum sort_order
-                       from (select distinct site_type value,
-                                             cast(null as varchar2(4000 char)) descr
+                       from (select /*+ parallel (4) */ 
+                                    distinct site_type code_value,
+                                             cast(null as varchar2(4000 char)) description
                                from station' || the_suffix || '
+                              where site_type is not null
                                 order by 1)';
   dbms_output.put_line('created table sitetype' || the_suffix);
 
