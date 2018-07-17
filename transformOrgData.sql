ee7f5ba008c5f651d9a2d751b709566941aaea4c
Pattermann <dpattermann@usgs.gov>
2018-07-17 11:58:52 -0500

    Make changes to get org_data into stewards_Swap

---
+++ transformOrgData.sql
@@ -0,0 +1,24 @@
+show user;
+select * from global_name;
+set timing on;
+set serveroutput on;
+whenever sqlerror exit failure rollback;
+whenever oserror exit failure rollback;
+select 'transform org_data start time: ' || systimestamp from dual;
+
+prompt building org_data_swap_stewards 
+
+prompt dropping stewards org_data indexes
+exec etl_helper_org_data.drop_indexes('stewards');
+
+set define off;
+prompt populating org_data_swap_stewards
+truncate table org_data_swap_stewards;
+insert /*+ append parallel(4) */
+  into org_data_swap_stewards (data_source_id, data_source, organization, organization_name)
+select DISTINCT /* parallel(4) */
+       1 data_source_id,
+	   'STEWARDS' data_source,
+	   organization,
+       organization_name
+  from STATION_SWAP_STEWARDS
